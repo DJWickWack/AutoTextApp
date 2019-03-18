562dba4d39b863c4d9187c5e4f53057ce832d917
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2019-03-18 08:55:35 -0400

    SQL table

--- AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
+++ AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.autotextapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the path and database name
+        String path="/data/data/"+getPackageName()+"/sample.db";
+
+        SQLiteDatabase db;
+        db= SQLiteDatabase.openOrCreateDatabase(path,null);
+        String sql = "CREATE TABLE IF NOT EXISTS info"+"(_ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,message TEXT,contact TEXT,platform TEXT, startdate TEXT,enddate TEXT, starttime TEXT, endtime TEXT );";
+        db.execSQL(sql);
+
+        ContentValues values= new ContentValues();
+        values.put("names","Bruce");
+        db.insert("people",null,values);
     }
 }
