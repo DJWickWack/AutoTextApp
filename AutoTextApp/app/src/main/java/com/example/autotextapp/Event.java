7bdc8c4882e8fd8bdbfa483a521af3bcb2b9aab5
connerj1 <connerj1@wit.edu>
2019-03-18 15:11:17 -0400

    Including piece to send a text(hardcoded to my phone number) on app open

---
+++ AutoTextApp/app/src/main/java/com/example/autotextapp/Event.java
@@ -0,0 +1,17 @@
+package com.example.autotextapp;
+
+import java.util.Date;
+
+public class Event {
+
+    int id;
+    String[] messages;
+    String phoneNumber;
+    String Message;
+    Date start;
+    Date end;
+
+    public Event(){
+
+    }
+}
--- AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
+++ AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.autotextapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("+19786978693", null, "Hi", null, null);
+
     }
 }
