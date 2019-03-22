a5bf638f08638ef8474a8d3de6eaf25ff3a16db8
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-03-22 14:03:42 -0400

    Basic Calendar Layout and Date Saving

--- AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
+++ AutoTextApp/app/src/main/java/com/example/autotextapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.autotextapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    CalendarView mainCalendar;
+    TextView selectedDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainCalendar = (CalendarView) findViewById(R.id.calendarView);
+        selectedDate = (TextView) findViewById((R.id.currentDate));
+
+        mainCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            String date = (month + 1) + "/" + dayOfMonth + "/" + year;
+            selectedDate.setText(date);
+            }
+        });
     }
 }
